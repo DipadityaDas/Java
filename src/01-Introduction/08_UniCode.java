daca5f24abc94886dab1a62614e6615a1dd5769b
DipadityaDas <DipadityaDas@gmail.com>
2023-09-06 21:45:03 +0530

    Bangla Independent Vowels

---
+++ src/01-Introduction/08_UniCode.java
@@ -0,0 +1,9 @@
+class UniCode {
+	public static void main( String[] args ) {
+
+		// Print all Independent Bengali Vowels
+		for (char c = 0x0985; c <= 0x0994; c++) {
+			System.out.print( c + " " );
+		}
+	}
+}
